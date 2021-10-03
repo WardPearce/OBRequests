9d56194f134a2e8203dd278f79357b8b5a8483ea
WardPearce <wardpearce@protonmail.com>
2021-10-03 20:41:05 +1300

    Started 2.0.0

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from OBRequests.tests.test_blocking import *  # noqa: F403, F401
-from OBRequests.tests.test_awaiting import *  # noqa: F403, F401
-
-
-if __name__ == "__main__":
-    unittest.main()
